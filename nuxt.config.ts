962e82247dd98d3b68b34eac5d2ed114da9782c8
niklhut <49069026+niklhut@users.noreply.github.com>
2025-06-18 22:27:19 +0200

    feat: add drizzle config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import { copyDrizzleMigrations } from './server/utils/nitro-hooks'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
@@ -20,6 +22,21 @@ export default defineNuxtConfig({
   },
 
   compatibilityDate: '2025-01-15',
+
+  nitro: {
+    experimental: {
+      tasks: true
+    },
+    imports: {
+      dirs: [
+        'server/db/*'
+      ]
+    }
+  },
+
+  hooks: {
+    'nitro:build:public-assets': copyDrizzleMigrations
+  },
 
   eslint: {
     config: {
